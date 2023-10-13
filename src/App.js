b778bb0f3be6e82da09c1e17108443a7eb21d06f
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-13 01:33:19 -0700

    Current move in list shows text not button

--- src/App.js
+++ src/App.js
@@ -68,14 +68,16 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description
-    if (move > 0) {
-      description = 'Go to move #' + move;
+    if (move === currentMove) {
+      description = 'You are at move #' + move
+    } else if (move > 0) {
+      description = 'Go to move #' + move
     } else {
       description = 'Go to game start';
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move === currentMove ? <span>{description}</span> : <button onClick={() => jumpTo(move)}>{description}</button>}
       </li>
     )
   })
