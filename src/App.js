40bc929b2e242c843e3ac0b3421ab2e28f4611a1
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-19 02:36:52 -0700

    add remove button with some error handling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GamesList from './Components/GamesList.jsx'
 
 export default function Home() {
   const [games, setGames] = useState([])
+  const [errorMsg, setErrorMsg] = useState("")
   const [newName, setNewName] = useState("")
 
   useEffect(() => {
@@ -20,15 +21,35 @@ export default function Home() {
       },
       body: JSON.stringify({name: newName}),
     }).then(res => res.json())
-    .then(({games:newGames}) => {
-      setGames(newGames)
-      setNewName("")
+    .then(({success, games:newGames, msg}) => {
+      if (success) {
+        setGames(newGames)
+        setNewName("")
+        setErrorMsg(null)
+      }
+      if (!success) {
+        setErrorMsg(msg)
+      }
     })
   }
+  
+  function handleRemoveGame(gameId) {
+      fetch(`http://localhost:5000/game/${gameId}`, {method: "DELETE"})
+        .then(res => res.json())
+        .then(({success, games:newGames, msg}) => {
+          if (success) {
+            setGames(newGames)
+            setErrorMsg(null)
+          }
+          if (!success) setErrorMsg(msg)
+        })
+  }
+
 
   return <>
+    {errorMsg ? <p className="error">{errorMsg}</p> : null}
     <input placeholder='New Game Name' value={newName} onChange={e => setNewName(e.target.value)} />
     <button onClick={handleCreateNewGame}>Create New Game</button>
-    <GamesList games={games} />
+    <GamesList games={games} handleRemoveGame={handleRemoveGame} />
   </>
 }
--- src/Components/GamesList.jsx
+++ src/Components/GamesList.jsx
@@ -1,9 +1,12 @@
-export default function GamesList({ games }) {
+export default function GamesList({ games, handleRemoveGame }) {
     return <>
         <ul>
             {games.map(game => {
                 return (
-                    <li key={game.game_id}>{game.name}</li>
+                    <li key={game.game_id} className="game-list-item">
+                        <button onClick={() => handleRemoveGame(game.game_id)}>Remove</button>
+                        <p>{game.name}</p>
+                    </li>
                 )
             })} 
         </ul>
