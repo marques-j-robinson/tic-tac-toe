1ebbcb58e04d46333aaba0097e5a487d7f816c4c
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-19 02:01:23 -0700

    add method for creating new game in system

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GamesList from './Components/GamesList.jsx'
 
 export default function Home() {
   const [games, setGames] = useState([])
+  const [newName, setNewName] = useState("")
 
   useEffect(() => {
     fetch('http://localhost:5000/games')
@@ -10,7 +11,24 @@ export default function Home() {
       .then(data => setGames(data))
   }, [])
 
+  function handleCreateNewGame(e) {
+    e.preventDefault()
+    fetch('http://localhost:5000/games', {
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({name: newName}),
+    }).then(res => res.json())
+    .then(({games:newGames}) => {
+      setGames(newGames)
+      setNewName("")
+    })
+  }
+
   return <>
+    <input placeholder='New Game Name' value={newName} onChange={e => setNewName(e.target.value)} />
+    <button onClick={handleCreateNewGame}>Create New Game</button>
     <GamesList games={games} />
   </>
 }
