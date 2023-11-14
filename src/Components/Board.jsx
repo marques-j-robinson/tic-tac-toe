8bdd11d6c0be04a3a89906c52f5d00655c7b6a7b
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-14 12:31:13 -0800

    display squares on a board in a single loop

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -41,20 +41,8 @@ export default function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      <div className="game-board-items">
+        {squares.map((_, i) => <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />)}
       </div>
     </>
   );
