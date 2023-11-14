96356d435bd58fe3e43a5e7af481830152413c45
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-14 07:37:27 -0800

    seperate square component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { useState } from 'react';
 
-function Square({value, onSquareClick}) {
-  return <button className="square" onClick={onSquareClick}>
-    {value}
-  </button>;
-}
+import Square from './Components/Square.jsx'
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
