a0b1918b23dbe58874bb3051807d192dc76fe502
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-14 10:07:58 -0800

    refactor move list display loop

--- src/Components/MoveList.jsx
+++ src/Components/MoveList.jsx
@@ -1,18 +1,17 @@
+function MoveListItem({move, jumpTo}) {
+  let description;
+  if (move > 0) {
+    description = 'Go to move #' + move;
+  } else {
+    description = 'Go to game start';
+  }
+  return <button onClick={() => jumpTo(move)}>{description}</button>
+}
+
 export default function MoveList({history, jumpTo}) {
-  const moves = history.map((squares, move) => {
-		let description;
-	    if (move > 0) {
-	      description = 'Go to move #' + move;
-	    } else {
-	      description = 'Go to game start';
-	    }
-	    return (
-	      <li key={move}>
-	        <button onClick={() => jumpTo(move)}>{description}</button>
-	      </li>
-	    );
-	});
-  return <>
-		<ol>{moves}</ol>
-	</>
+  return <ol>{history.map((square, move) => (
+    <li key={move}>
+      <MoveListItem move={move} jumpTo={jumpTo} />
+    </li>
+  ))}</ol>
 }
