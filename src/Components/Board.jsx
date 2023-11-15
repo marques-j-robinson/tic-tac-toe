148f0b93f05e896933b6dde1d537245ad94a69ae
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-15 00:35:05 -0800

    Refactor calculateWinner to also return information about what victory line won

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,20 +1,21 @@
 import Square from './Square.jsx'
 
+const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {winner: squares[a], linesIdx: i};
     }
   }
   return null;
@@ -31,10 +32,13 @@ export default function Board({ xIsNext, squares, onPlay }) {
     }
     onPlay(nextSquares);
   }
-  const winner = calculateWinner(squares);
+  const isGameOver = calculateWinner(squares);
   let status;
-  if (winner) {
+  let victoryLines
+  if (isGameOver) {
+    const {winner, linesIdx} = isGameOver
     status = "Winner: " + winner;
+    victoryLines = lines[linesIdx]
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
