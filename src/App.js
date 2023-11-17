92013bfbdc05a7e624cf36e198795976751b872b
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-17 00:19:52 -0800

    setup location mappings data and locations state

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import { useState } from 'react';
 import Board from './Components/Board.jsx'
 import MoveList from './Components/MoveList.jsx'
 
+const locationMappings = [
+    '0,0',
+    '0,1',
+    '0,2',
+    '1,0',
+    '1,1',
+    '1,2',
+    '2,0',
+    '2,1',
+    '2,2',
+]
+
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [locations, setLocations] = useState([])
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
   function handlePlay(nextSquares) {
