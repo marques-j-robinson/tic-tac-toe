5c60d4e16e593645b46d9d42a8099a4e8fdb866f
marques-j-robinson <marques.j.robinson@gmail.com>
2023-12-30 03:08:35 -0800

    reset repo

--- src/Components/Square.jsx
+++
@@ -1,5 +0,0 @@
-export default function Square({value, onSquareClick, highlight}) {
-  return <button className={`square ${highlight?'highlight':null}`} onClick={onSquareClick}>
-    {value}
-  </button>;
-}
